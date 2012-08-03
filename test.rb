635571143a88368b3a11b8b9c21bac6a500dd5c0
Indro De <indro.de@gmail.com>
2012-08-03 18:46:36 +0200

    Add some basic error handling

--- test.rb
+++ test.rb
@@ -2,17 +2,28 @@ require 'sinatra'
 require 'redcarpet'
 require 'haml'
 
-get '/hi' do
-  "Hello World!"
-end
-
+set :environment, :production
 set :haml, :layout_engine => :haml, :layout => :index
 set :markdown, :layout_engine => :haml, :layout => :index
 
 get '/' do
+  @message = "It is #{Time.now}."
   haml :home
 end
 
 get '/:article' do
-  markdown File.read("articles/" + params[:article] + ".markdown")
+  begin
+    article = File.read("articles/#{params[:article]}.markdown")
+    markdown article
+  rescue
+    not_found
+  end
+end
+
+not_found do
+  haml :'404'
+end
+
+error do
+  haml :'500'
 end
