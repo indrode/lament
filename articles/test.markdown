fe8f125718b2f40625ea78d586b78d7ddc38b740
Indro De <indro.de@gmail.com>
2012-08-03 18:34:42 +0200

    Add some test views and functionality

---
+++ articles/test.markdown
@@ -0,0 +1,3 @@
+## This is a header
+
+This is some regular markdown copy. And (this is a link)[http://indrode.com/].
--- test.rb
+++ test.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'redcarpet'
+require 'haml'
 
 get '/hi' do
   "Hello World!"
+end
+
+set :haml, :layout_engine => :haml, :layout => :index
+set :markdown, :layout_engine => :haml, :layout => :index
+
+get '/' do
+  haml :home
+end
+
+get '/:article' do
+  markdown File.read("articles/" + params[:article] + ".markdown")
 end
