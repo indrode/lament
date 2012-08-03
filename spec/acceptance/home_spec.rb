99bccefdc6add34402ac8ba9a68f7bab70607536
Indro De <indro.de@gmail.com>
2012-08-03 18:49:31 +0200

    Add test for 404 error handling

--- spec/acceptance/home_spec.rb
+++ spec/acceptance/home_spec.rb
@@ -11,5 +11,10 @@ describe "/" do
     page.should have_no_content('abracadabra')
   end
 
+  it "should display error page if article was not found" do
+    visit '/some_missing_article'
+    page.should have_content('Error 404')
+  end
+
 end
 
