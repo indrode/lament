60871f137ca79fb597b4d2e868c99b4cf290ea77
Indro De <indro.de@gmail.com>
2013-02-22 15:04:03 +0100

    Add slider to be implemented later

--- public/js/base.js
+++ public/js/base.js
@@ -1,4 +1,13 @@
 jQuery(document).ready(function($) {
   // insert jquery magic here
+
+  /* simple slider functionality to be integrated at a later point in time
+  $('#slider-wrapper').hover(function() {  
+    $('#robot-whaat').animate({ height: '56px' });
+  },
+  function() {
+    $('#robot-whaat').animate({ height: '22px' }); 
   });
+  */
+
 });
