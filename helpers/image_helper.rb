81e95c1b768d9f7e348b8acc8d47c4313c6c8c1c
Indro De <indro.de@gmail.com>
2013-03-06 11:06:59 +0100

    Implement view image helper

---
+++ helpers/image_helper.rb
@@ -0,0 +1,8 @@
+def image_with_exif(file)
+  path = "#{File.dirname(__FILE__)}/../public/photos/#{file}.jpg"
+  exposure = EXIFR::JPEG.new(path).exposure_time.to_s.gsub(/\/1/, '')
+  f_number = EXIFR::JPEG.new(path).f_number.to_f
+  iso_speed = EXIFR::JPEG.new(path).exif.iso_speed_ratings
+
+  "<small>#{exposure}s f#{f_number} ISO#{iso_speed}</small><br /><img src='/photos/#{file}.jpg' class='wodom' width='710' alt='' /><br />"
+end
---
+++ spec/helpers/image_helper_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require File.join(ROOT, 'helpers/image_helper.rb')
+
+describe "#image_with_exif" do
+  let(:file) { "some_path" }
+  let(:exif_data) { mock("exif", :iso_speed_ratings => 400) }
+  let(:photo) { mock("photo", :exposure_time => 2.0, :f_number => 1.8, :exif => exif_data) }
+
+  it "#image_with_exif displays img tag and exif info" do
+    EXIFR::JPEG.stub(:new).and_return photo
+
+    image_with_exif(file).should == "<img src='/photos/some_path.jpg' class='wodom' width='710' alt='' /><br />2.0s f1.8 ISO400"
+  end
+end
