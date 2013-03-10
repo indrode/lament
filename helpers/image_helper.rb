aa5e7a22c9f113ef4462a002f866a0387fdcf875
Indro De <indro.de@gmail.com>
2013-03-10 14:29:21 +0100

    Add date_time from photo EXIF

--- helpers/image_helper.rb
+++ helpers/image_helper.rb
@@ -3,6 +3,8 @@ def image_with_exif(file)
   exposure = EXIFR::JPEG.new(path).exposure_time.to_s.gsub(/\/1$/, '')
   f_number = EXIFR::JPEG.new(path).f_number.to_f
   iso_speed = EXIFR::JPEG.new(path).exif.iso_speed_ratings
+  date_time = EXIFR::JPEG.new(path).exif.date_time.to_s
 
-  "<small>#{exposure}s f#{f_number} ISO#{iso_speed}</small><br /><img src='/photos/#{file}.jpg' class='wodom' width='710' alt='' /><br />"
+  date_time = date_time.match(/.*T/).to_s.chop
+  "<small>#{exposure}s f#{f_number} ISO#{iso_speed} #{date_time}</small><br /><img src='/photos/#{file}.jpg' class='wodom' width='710' alt='' /><br />"
 end
--- spec/helpers/image_helper_spec.rb
+++ spec/helpers/image_helper_spec.rb
@@ -3,12 +3,12 @@ require File.join(ROOT, 'helpers/image_helper.rb')
 
 describe "#image_with_exif" do
   let(:file) { "some_path" }
-  let(:exif_data) { mock("exif", iso_speed_ratings: 400) }
+  let(:exif_data) { mock("exif", iso_speed_ratings: 400, date_time: "2006-10-15T11:05:44-04:00") }
   let(:photo) { mock("photo", exposure_time: 2.0, f_number: 1.8, exif: exif_data) }
 
   it "#image_with_exif displays img tag and exif info" do
     EXIFR::JPEG.stub(:new).and_return photo
 
-    image_with_exif(file).should == "<small>2.0s f1.8 ISO400</small><br /><img src='/photos/some_path.jpg' class='wodom' width='710' alt='' /><br />"
+    image_with_exif(file).should == "<small>2.0s f1.8 ISO400 2006-10-15</small><br /><img src='/photos/some_path.jpg' class='wodom' width='710' alt='' /><br />"
   end
 end
