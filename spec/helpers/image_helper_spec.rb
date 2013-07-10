781c744b062141b37acead6b5cd35dd3611ed0e9
Indro De <indro.de@gmail.com>
2013-07-10 15:08:05 +0200

    Replace 'mock' with 'double'

--- spec/helpers/image_helper_spec.rb
+++ spec/helpers/image_helper_spec.rb
@@ -3,8 +3,8 @@ require File.join(ROOT, 'helpers/image_helper.rb')
 
 describe "#image_with_exif" do
   let(:file) { "some_path" }
-  let(:exif_data) { mock("exif", iso_speed_ratings: 400, date_time: "2006-10-15T11:05:44-04:00") }
-  let(:photo) { mock("photo", exposure_time: 2.0, f_number: 1.8, exif: exif_data) }
+  let(:exif_data) { double("exif", iso_speed_ratings: 400, date_time: "2006-10-15T11:05:44-04:00") }
+  let(:photo) { double("photo", exposure_time: 2.0, f_number: 1.8, exif: exif_data) }
 
   it "#image_with_exif displays img tag and exif info" do
     EXIFR::JPEG.stub(:new).and_return photo
