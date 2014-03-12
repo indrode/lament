159ed5765165ac3e855a1bd94d2cc7393043d1fa
Indro De <indro.de@gmail.com>
2014-03-12 15:14:40 +0100

    Adjust config/puma.rb for Mina deploys

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,11 @@
 root = "#{Dir.getwd}"
 daemonize true
-bind "unix://#{root}/tmp/puma/socket"
-pidfile "#{root}/tmp/puma/pid"
-state_path "#{root}/tmp/puma/state"
+bind "unix://#{root}/../../tmp/puma/socket"
+pidfile "#{root}/../../tmp/puma/pid"
+state_path "#{root}/../../tmp/puma/state"
 rackup "#{root}/config.ru"
 
-threads 4, 8
+threads 2, 4
 
 activate_control_app
+
